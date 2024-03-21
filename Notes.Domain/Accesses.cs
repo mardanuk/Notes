d634f9496639b2a732bded2ecf021cf7b9d9fdc1
Мадина Зайнуллина <madinazay@mail.ru>
2024-03-21 16:26:53 +0300

    develop. Новые сущности

---
+++ Notes.Domain/Accesses.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Notes.Domain
+{
+    [Table("accesses")]
+    internal class Accesses
+    {
+        [Key]
+        [Column("login")]
+        public required string Login { get; set; }
+        
+        [Key]
+        [Column("header")]
+        public required string Header { get; set; }
+    }
+}
---
+++ Notes.Domain/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Notes.Domain
+{
+    [Table("users")]
+    internal class User
+    {
+        [Key]
+        [Column("login")]
+        public required string Login { get; set; }
+
+        [Column("name")]
+        public required string Name { get; set; }
+    }
+}
